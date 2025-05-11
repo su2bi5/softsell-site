cc5a41af60a8c8b1645a8bcbf660acafd51a9908
su2bi5 <112824577+su2bi5@users.noreply.github.com>
2025-05-11 23:11:11 +0530

    Add files via upload

---
+++ postcss.config.mjs
@@ -0,0 +1,9 @@
+const config = {
+  plugins: {
+    'postcss-nesting': {},          // Use the updated nesting plugin
+    tailwindcss: {},                // Use the correct Tailwind CSS plugin for PostCSS
+    autoprefixer: {},               // Optional: for browser compatibility
+  },
+};
+
+export default config;
